9651f727af46e08c833a9b9fd6ec46c84d457581
Mert Akel <makel@fonetyazilim.com>
2018-07-02 14:04:39 +0300

    first commit

---
+++ src/main/java/com/mertakl/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.mertakl.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mertakl.springmvc")
+public class HelloWorldConfiguration {
+	
+
+}
